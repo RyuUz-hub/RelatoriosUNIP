9ecd806b369d6f2377c88c588e70528d511df881
RyuUz <abnercoelho420@gmail.com>
2025-11-07 16:51:54 -0300

    relatorio_v1

---
+++ Relatorio4.py
@@ -0,0 +1,8 @@
+cont = 0
+while cont < 5:
+    if ((int(input('Digite um número: ')) % 2) == 0):
+        print('Número par!!!')
+        cont+= 1
+    else:
+        print()
+print('Você digitou 5 números pares!!!')
