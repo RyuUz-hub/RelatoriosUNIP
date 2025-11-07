c556d2ec825aa89afe034fb4da9a682524edb82e
RyuUz <abnercoelho420@gmail.com>
2025-11-07 17:11:17 -0300

    LoopV_F

--- Relatorio2_4.py
+++ Relatorio2_4.py
@@ -1,2 +1,5 @@
 while True:
     print(str(bin(int(input('Digite um número para ter sua versão binária: '))))[2:])
+    if str(input('Deseja continuar S/N: ')).lower()[0] == 'n':
+        print('Até mais!')
+        break
